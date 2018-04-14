4180a7efa4a4c90ff543ba2087c633a305c8870e
jay <jay@jaydeMacBook-Pro.local>
2018-04-14 14:33:33 +0800

    new file:   db_op.py 	modified:   migrate.py 	modified:   server.py 	copied:     migrate.py -> setting.py

--- server.py
+++ server.py
@@ -14,12 +14,11 @@ if __name__ ==  "__main__":
     	sa =httpd.socket.getsockname()
     	print 'http://{0}:{1}/'.format(*sa)
     	httpd.serve_forever()
-  elif 'createdatabase' in sys.argv:         #创建数据库
+  elif 'createdb' in sys.argv:         #创建数据库,创建表
       from  setting import db
-      db.create()
-  elif 'migrate' in sys.argv:                #同步数据库
-      from migrate import migration
-      migration.create_Table(blog,blog_list.post,blog_list.comment)
+      db.create_db()
+      db.create_Table(blog,blog_list.post,blog_list.comment)              
+
 
   else:
 	print "Less args!!!\n"
--- setting.py
+++ setting.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-
+from admin   import blog_list
 import MySQLdb
 
 class _db(object):
@@ -9,9 +9,10 @@ class _db(object):
 		self.user = 'root'
 		self.passwd = 'wuwangjie'
 		self.db_name = 'blog'
+		self.table_name = ''
 
 	
-	def create(self):
+	def create_db(self):
 		try:
 			db     = MySQLdb.connect(self.host, self.user, self.passwd)
 			cursor = db.cursor()
@@ -26,6 +27,50 @@ class _db(object):
 		finally:
 		# 关闭数据库连接
 			db.close()
+
+	def create_Table(self,app,*args):
+
+		try:
+			try:
+				db     = MySQLdb.connect(self.host, self.user, self.passwd)
+				cursor = db.cursor() 
+				cursor.execute('use '+self.db_name)
+				print self.db_name
+		
+			except MySQLdb.Error, e:
+				cursor.rollback()
+				db.rollback()
+				print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+
+
+			try:
+				for t in range(len(args)):                                 #不定参数个数
+					self.table_name = blog_list.table[t]				   #获取表名
+					sql_table="""
+							  CREATE TABLE `""" + self.table_name + """` 
+							  (
+							  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+							   PRIMARY KEY (`id`)
+							  ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+						  """
+					cursor.execute(sql_table)
+					for t_value in range(len(args[t])):						#args[t]为不定参数名
+						#print len(args[0])
+						vals=getattr(app,args[t][t_value])					#获取表属性数据类型
+						sql_add="alter table "+self.table_name+" add column "+args[t][t_value]+" "+vals+" NOT NULL"  #为表增加一列
+						cursor.execute(sql_add)
+			except MySQLdb.Error, e:
+				cursor.rollback()
+				db.rollback()
+				print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+		except MySQLdb.Error,e:
+			cursor.rollback()
+			db.rollback()
+			print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+
+		db.commit()
+		cursor.close()
+		db.close()	
 
 
 db = _db()
